78b19d3ecdf9c899bd6e5a38a1d813f827d5c633
check <a1784491710@126.com>
2018-08-06 13:59:32 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String tmperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+
+}
